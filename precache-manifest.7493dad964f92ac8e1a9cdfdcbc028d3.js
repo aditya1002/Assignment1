a44c75a42d9269b35a870cb55735dc0c9a9a6c00
Aditya Bharadwaj <mail2adityabharadwaj@gmail.com>
2024-06-21 01:33:56 +0530

    Updates

---
+++ precache-manifest.7493dad964f92ac8e1a9cdfdcbc028d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4905d3ae1821401dd01572ceb5be767",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e21fb999f2652db8efbc",
+    "url": "/static/css/main.87422237.chunk.css"
+  },
+  {
+    "revision": "6c3d908548d5d38d2092",
+    "url": "/static/js/2.bb9376a2.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "e21fb999f2652db8efbc",
+    "url": "/static/js/main.9d182bbb.chunk.js"
+  },
+  {
+    "revision": "9693e09b1248ae468088",
+    "url": "/static/js/runtime~main.051a30a6.js"
+  }
+]);
